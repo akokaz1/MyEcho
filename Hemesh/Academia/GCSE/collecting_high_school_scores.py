d15ad1591c3d207a5eafb8060806f04d4e7730b8
hemesh-patel <hemeshpatel@hotmail.co.uk>
2017-03-08 11:50:27 +0000

    final data sorted out for the calculations

--- Hemesh/Academia/GCSE/collecting_high_school_scores.py
+++ Hemesh/Academia/GCSE/collecting_high_school_scores.py
@@ -1,5 +1,4 @@
-'''script opens files which I collected into a text file from the web.
-Aim is to collect the name of the school and the attainment score'''
+'''script loops through the files in the folder 'school_ranking_raw_data' and collects the school name and school score'''
 
 import pandas as pd
 import os
@@ -7,7 +6,10 @@ from sklearn import preprocessing
 
 schoolname = []
 schoolscore = []
+
+
 def collection (file):
+    '''Function is run on individual files and collects the schoolname and attainment score and appends it to the two lists above'''
     with open(file, 'r') as fhand:
         for line in fhand:
             if not line.startswith('Type'):
@@ -34,18 +36,19 @@ def collection (file):
                     line = line.split()
                     schoolscore.append(line[6])
 
+# This block loops there the files in the school_ranking_raw_data and collects the names of the files
 files = []
-path = 'C:\Users\student01\Desktop\MyEcho\Academia\GCSE\school ranking'
+path = 'C:\Users\student01\Desktop\MyEcho\Academia\GCSE\school_ranking_raw_data'
 lst = [x[2] for x in os.walk(path)]
 for l in lst:
     for file_name in l:
         files.append(file_name)
-
+# This block loops through the files and runs the above function
 for file in files:
-    collection('C:\Users\student01\Desktop\MyEcho\Academia\GCSE\school ranking\\' + file)
+    collection('C:\Users\student01\Desktop\MyEcho\Academia\GCSE\school_ranking_raw_data\\' + file)
 
 
-
+# Create a dataframe of the two lists, normalise the attainment score and save the df as csv file.
 df = pd.DataFrame(
     {'SchoolName': schoolname,
     'SchoolScore': schoolscore})
